59b84c96a2c0d249bc8e0b301d8797001c79082a
Mount <wtao565@connect.hkust-gz.edu.cn>
2026-05-05 16:14:24 +0800

    fix: correct scene media asset path

--- lib/demo-scenes.ts
+++ lib/demo-scenes.ts
@@ -34,7 +34,7 @@ export const demoScenes: DemoScene[] = [
         text: "啊啊啊是的！我老喜欢秋秋了",
         sentAt: "10:12",
         media: {
-          src: "/bridgechat/qiuqiu.jpg",
+          src: "/bridgechat/bridgechat/qiuqiu.jpg",
           alt: "秋秋的照片",
           caption: "秋秋",
         },
